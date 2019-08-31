96f461b43b9fa3e3641b564969040394b87e488b
HYEON(iMac) <antaehyeon@live.co.kr>
2019-09-01 01:08:53 +0900

    Feat: TV Container 서버 요청(GET)로직 추가

--- src/Routes/TV/TVContainer.js
+++ src/Routes/TV/TVContainer.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import TVPresenter from "Routes/TV/TVPresenter.js";
+import { tvAPI } from "api.js";
 
 export default class TVContainer extends Component {
   state = {
@@ -10,7 +11,31 @@ export default class TVContainer extends Component {
     error: null
   };
 
+  async componentDidMount() {
+    try {
+      const {
+        data: { results: topRated }
+      } = await tvAPI.topRated();
+      const {
+        data: { results: popular }
+      } = await tvAPI.popular();
+      const {
+        data: { results: airingToday }
+      } = await tvAPI.airingToday();
+
+      this.setState({ topRated, popular, airingToday });
+    } catch {
+      this.setState({
+        error: "Can't find TV information."
+      });
+    } finally {
+      this.setState({ loading: false });
+    }
+  }
+
   render() {
+    console.log(this.state);
+
     const { topRated, popular, airingToday, loading, error } = this.props;
     return <TVPresenter topRated={topRated} popular={popular} airingToday={airingToday} loading={loading} error={error} />;
   }
