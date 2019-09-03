8a23f6b004574d2088496e136143847039fae184
Hyeon <antaehyeon@live.co.kr>
2019-09-04 01:03:18 +0900

    Feat: Search Container 및 Presenter 로직 추가

--- src/Routes/Home/HomePresenter.js
+++ src/Routes/Home/HomePresenter.js
@@ -5,7 +5,7 @@ import styled from "styled-components";
 import Section from "Components/Section";
 
 const Container = styled.div`
-  padding: 0px 10px;
+  padding: 0px 20px;
 `;
 
 const HomePresenter = ({ nowPlaying, popular, upcoming, loading, error }) =>
--- src/Routes/Search/SearchContainer.js
+++ src/Routes/Search/SearchContainer.js
@@ -6,25 +6,33 @@ export default class SearchContainer extends Component {
   state = {
     movieResults: null,
     tvResults: null,
-    searchTerm: "code",
+    searchTerm: "",
     loading: false,
     error: null
   };
 
-  componentDidMount() {
-    this.handleSubmit();
-  }
-
-  handleSubmit = () => {
+  handleSubmit = event => {
+    event.preventDefault();
     const { searchTerm } = this.state;
     if (searchTerm !== "") {
       this.searchByTerm(searchTerm);
     }
   };
 
+  updateTerm = event => {
+    const {
+      target: { value }
+    } = event;
+
+    this.setState({
+      searchTerm: value
+    });
+  };
+
   searchByTerm = async term => {
     const { searchTerm } = this.state;
     try {
+      this.setState({ loading: true });
       const {
         data: { results: movieResults }
       } = await moviesAPI.search(searchTerm);
@@ -32,8 +40,7 @@ export default class SearchContainer extends Component {
         data: { results: tvResults }
       } = await tvAPI.search(searchTerm);
       console.log("[Search Container] searchByTerm results", { movieResults, tvResults });
-
-      this.setState({ loading: true });
+      this.setState({ movieResults, tvResults });
     } catch {
       this.setState({ error: "Can't find results." });
     } finally {
@@ -42,7 +49,17 @@ export default class SearchContainer extends Component {
   };
 
   render() {
-    const { movieResults, tvResults, searchTerm, loading, error } = this.props;
-    return <SearchPresenter movieResults={movieResults} tvResults={tvResults} searchTerm={searchTerm} loading={loading} error={error} handleSubmit={this.handleSubmit} />;
+    const { movieResults, tvResults, searchTerm, loading, error } = this.state;
+    return (
+      <SearchPresenter
+        movieResults={movieResults}
+        tvResults={tvResults}
+        searchTerm={searchTerm}
+        loading={loading}
+        error={error}
+        handleSubmit={this.handleSubmit}
+        updateTerm={this.updateTerm}
+      />
+    );
   }
 }
