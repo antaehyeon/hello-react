e64472261fa177d3132dad1eb297a84c6035f7e8
Hyeon <antaehyeon@live.co.kr>
2019-09-09 23:01:51 +0900

    Feat: React-Helmet을 이용한 웹페이지 헤더 텍스트 변경

--- src/Routes/Home/HomePresenter.js
+++ src/Routes/Home/HomePresenter.js
@@ -4,66 +4,76 @@ import styled from "styled-components";
 import Section from "Components/Section";
 import Message from "Components/Message.js";
 import Poster from "Components/Poster.js";
+import Helmet from "react-helmet";
+import Loader from "Components/Loader.js";
 
 const Container = styled.div`
   padding: 20px;
 `;
 
-const HomePresenter = ({ nowPlaying, popular, upcoming, loading, error }) =>
-  loading ? null : (
-    <Container>
-      {console.log("[Home Presenter] props", { nowPlaying, popular, upcoming, loading, error })}
-      {nowPlaying && nowPlaying.length > 0 && (
-        <Section title="Now Playing">
-          {nowPlaying.map(movie => (
-            // <span key={movie.id}>{movie.title}</span>
-            <Poster
-              key={movie.id}
-              id={movie.id}
-              imageUrl={movie.poster_path}
-              title={movie.original_title}
-              rating={movie.vote_average}
-              year={movie.release_date.substring(0, 4)}
-              isMovie={true}
-            />
-          ))}
-        </Section>
-      )}
-      {upcoming && upcoming.length > 0 && (
-        <Section title="Upcoming Movies">
-          {upcoming.map(movie => (
-            // <span key={movie.id}>{movie.title}</span>
-            <Poster
-              key={movie.id}
-              id={movie.id}
-              imageUrl={movie.poster_path}
-              title={movie.original_title}
-              rating={movie.vote_average}
-              year={movie.release_date.substring(0, 4)}
-              isMovie={true}
-            />
-          ))}
-        </Section>
-      )}
-      {popular && popular.length > 0 && (
-        <Section title="Popular Movies">
-          {popular.map(movie => (
-            // <span key={movie.id}>{movie.title}</span>
-            <Poster
-              key={movie.id}
-              id={movie.id}
-              imageUrl={movie.poster_path}
-              title={movie.original_title}
-              rating={movie.vote_average}
-              year={movie.release_date.substring(0, 4)}
-              isMovie={true}
-            />
-          ))}
-        </Section>
-      )}
-      {error && <Message color="#e743c3" text={error} />}
-    </Container>
-  );
+const HomePresenter = ({ nowPlaying, popular, upcoming, loading, error }) => (
+  <>
+    <Helmet>
+      <title>Movies / Nomflix</title>
+    </Helmet>
+    {loading ? (
+      <Loader />
+    ) : (
+      <Container>
+        {console.log("[Home Presenter] props", { nowPlaying, popular, upcoming, loading, error })}
+        {nowPlaying && nowPlaying.length > 0 && (
+          <Section title="Now Playing">
+            {nowPlaying.map(movie => (
+              // <span key={movie.id}>{movie.title}</span>
+              <Poster
+                key={movie.id}
+                id={movie.id}
+                imageUrl={movie.poster_path}
+                title={movie.original_title}
+                rating={movie.vote_average}
+                year={movie.release_date.substring(0, 4)}
+                isMovie={true}
+              />
+            ))}
+          </Section>
+        )}
+        {upcoming && upcoming.length > 0 && (
+          <Section title="Upcoming Movies">
+            {upcoming.map(movie => (
+              // <span key={movie.id}>{movie.title}</span>
+              <Poster
+                key={movie.id}
+                id={movie.id}
+                imageUrl={movie.poster_path}
+                title={movie.original_title}
+                rating={movie.vote_average}
+                year={movie.release_date.substring(0, 4)}
+                isMovie={true}
+              />
+            ))}
+          </Section>
+        )}
+        {popular && popular.length > 0 && (
+          <Section title="Popular Movies">
+            {popular.map(movie => (
+              // <span key={movie.id}>{movie.title}</span>
+              <Poster
+                key={movie.id}
+                id={movie.id}
+                imageUrl={movie.poster_path}
+                title={movie.original_title}
+                rating={movie.vote_average}
+                year={movie.release_date.substring(0, 4)}
+                isMovie={true}
+              />
+            ))}
+          </Section>
+        )}
+        {error && <Message color="#e743c3" text={error} />}
+      </Container>
+    )}
+  </>
+);
 
 HomePresenter.propTypes = {
   nowPlaying: PropTypes.array,
--- src/api.js
+++ src/api.js
@@ -1,43 +1,35 @@
 import axios from "axios";
 
-const api = axios.create({
-  baseURL: "https://api.themoviedb.org/3/",
+const commonAPIParams = {
   params: {
     api_key: "8fdf985d69ddae89b7bce0b75cbd30be",
     language: "ko-KR"
   }
+};
+
+const api = axios.create({
+  baseURL: "https://api.themoviedb.org/3/"
 });
 
 export const moviesAPI = {
-  nowPlaying: () => api.get("movie/now_playing"),
-  upcoming: () => api.get("movie/upcoming"),
-  popular: () => api.get("movie/popular"),
-  movieDetail: id =>
-    api.get(`movie/${id}`, {
-      params: {
-        append_to_response: "videos",
-        api_key: "8fdf985d69ddae89b7bce0b75cbd30be"
-      }
-    }),
+  nowPlaying: () => api.get("movie/now_playing", commonAPIParams),
+  upcoming: () => api.get("movie/upcoming", commonAPIParams),
+  popular: () => api.get("movie/popular", commonAPIParams),
+  movieDetail: id => api.get(`movie/${id}`, commonAPIParams),
   search: term =>
     api.get("search/movie", {
       params: {
+        api_key: "8fdf985d69ddae89b7bce0b75cbd30be",
         query: encodeURIComponent(term)
       }
     })
 };
 
 export const tvAPI = {
-  topRated: () => api.get("tv/top_rated"),
-  popular: () => api.get("tv/popular"),
-  airingToday: () => api.get("tv/airing_today"),
-  showDetail: id =>
-    api.get(`tv/${id}`, {
-      params: {
-        append_to_response: "videos",
-        api_key: "8fdf985d69ddae89b7bce0b75cbd30be"
-      }
-    }),
+  topRated: () => api.get("tv/top_rated", commonAPIParams),
+  popular: () => api.get("tv/popular", commonAPIParams),
+  airingToday: () => api.get("tv/airing_today", commonAPIParams),
+  showDetail: id => api.get(`tv/${id}`, commonAPIParams),
   search: term =>
     api.get("search/tv", {
       params: {
